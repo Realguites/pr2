16b784dcc98a6b73e7e4301403f5e31d2763e6f8
Gustavo Loper <theloper7@gmail.com>
2020-11-11 22:44:07 -0300

    :bug: Enviando a function soma

---
+++ Calculadora.js
@@ -0,0 +1,15 @@
+function sum(a,b){
+    return a + b;
+}
+
+function sub(){
+    //TODO: Implementar subtração
+}
+
+function mult(){
+    //TODO: Implementar multiplicação
+}
+
+function div(){
+    //TODO: Implementar divisão
+}
